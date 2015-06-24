03252c2285f3b7a5c9828f60b8fffb4bc8ba2c58
Joey Poon <joey@joeypoon.com>
2015-06-24 13:36:49 -0500

    show user

--- back_end/app/controllers/users_controller.rb
+++ back_end/app/controllers/users_controller.rb
@@ -5,10 +5,14 @@ class UsersController < ApplicationController
     @user.name = Faker::Commerce.color
     @user.token = SecureRandom.hex(7)
     if @user.save
-      render json: { success: 'yay' }
+      render :show, status: 201
     else
       render json: { error: 'Fail' }, status: 422
     end
+  end
+
+  def show
+    @user = User.find_by email: user_params[:email]
   end
 
   private
